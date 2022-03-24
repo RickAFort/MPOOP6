cfe081a75a80b1190ab1445392184aeace47013d
RickAFort <101783257+RickAFort@users.noreply.github.com>
2022-03-23 21:19:08 -0600

    Creación de Práctica 6

---
+++ src/controlador/Sumadora.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *Clase que suma numeros
+ * @author alumno
+ */
+public class Sumadora {
+    /**
+     * Constructor vacio
+     */
+    public Sumadora(){
+    
+    
+    }
+    
+    /**
+     * Metodo que realiza una suma de de sos numeros
+     * @param a El primer numero a sumar
+     * @param b El segundo numero a sumar
+     * @return La suma de dos valores double
+     */
+    public double sumar(double a,double b){
+    double suma= a + b;
+    return suma;
+    }
+    
+}
